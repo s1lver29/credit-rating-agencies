914acce9bf71d8abe2ec850fdc97da1d8b4c849c
s1lver29 <nikitaselver@yandex.ru>
2024-05-31 03:28:43 +0300

    Edit configuration and connection db

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -2,7 +2,6 @@ from enum import Enum
 from os import getenv
 from pathlib import Path
 
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,59 +10,21 @@ class Environment(str, Enum):
     prod = "prod"
 
 
-class DatabaseSettings(BaseSettings):
+class GlobalSettings(BaseSettings):
     env: Environment
     db_url: None | str = None
-    # db_host: None | int = None
-    # db_port: None | str = None
-    # db_user: None | str = None
-    # db_password: None | str = None
+    path_tokenizer: None | str = None
+    path_model: None | str = None
+    broker_redis_url: None | str = None
+    backend_redis_url: None | str = None
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent / f"{getenv('ENVIRONMENT')}.env",
         env_file_encoding="utf-8",
     )
-
-
-class ModelSettings(BaseSettings):
-    path_tokenizer: None | str = Field(None, env="PATH_TOKENIZER")
-    path_model: None | str = Field(None, env="PATH_MODEL")
-
-    model_config = SettingsConfigDict(
-        env_file=Path(__file__).parent / f"{getenv('ENVIRONMENT')}.env",
-        env_file_encoding="utf-8",
-    )
-
-
-# class CelerySettings(BaseSettings):
-#     broker_redis_url: None | str = None
-#     backend_redis_url: None | str = None
-
-#     model_config = SettingsConfigDict(
-#         env_file=Path(__file__).parent / f"{getenv('ENVIRONMENT')}.env",
-#         env_file_encoding="utf-8",
-#     )
-
-
-class Settings:
-    def __init__(self, env: Environment):
-        self.db_settings = DatabaseSettings(env=env)
-        self.model_settings = ModelSettings()
-        # self.celery_settings = CelerySettings()
-
-    def get_db_settings(self):
-        return self.db_settings
-
-    def get_model_settings(self):
-        return self.model_settings
-
-    # def get_celery_settings(self):
-    #     return self.celery_settings
 
 
 environment = getenv("ENVIRONMENT")
 if environment is None:
     raise ValueError("Environment variable 'ENVIRONMENT' is not set.")
-settings = Settings(env=environment)
-
-print(settings.get_db_settings())
+settings = GlobalSettings(env=environment)  # type: ignore
--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,14 +1,12 @@
-from os import getenv
-
-from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-load_dotenv()
+from conf.settings import settings
 
-environment = getenv("ENVIRONMENT")
-database_url = getenv("DATABASE_URL")
+database_url: str | None = settings.db_url
+
+if database_url is None:
+    raise ValueError("DATABASE_URL must not be None")
 
 engine = create_async_engine(database_url)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
-
